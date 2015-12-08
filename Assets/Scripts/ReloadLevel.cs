ced3eeb940170f25af4aa8b9039f0b1081c1797c
jmeggesto <jackie.meggesto@gmail.com>
2015-12-08 12:41:29 -0500

    finalized version

---
+++ Assets/Scripts/ReloadLevel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReloadLevel : MonoBehaviour {
+
+	public void ReloadGame()
+	{
+
+		Application.LoadLevel (1);
+
+	}
+
+	public void ReturnToMenu()
+	{
+
+		Application.LoadLevel (0);
+
+	}
+
+}
